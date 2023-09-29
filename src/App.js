7da64d58ad4e3b5efb1ea0005db35c9175970531
Rushikesh Acharya <v-racharya@microsoft.com>
2023-09-29 20:01:05 +0530

    feat: Theme Switcher implementation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Header from './Components/Header';
+import ThemeContext from './Context/ThemeContext';
+import HeroSection from './Components/HeroSection';
 import './App.css';
 
 function App() {
+  const themeHook = useState("light")    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={themeHook}>
+      <div>
+        <Header />
+        <HeroSection />
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
